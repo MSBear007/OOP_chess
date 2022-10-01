ea406657f80e320544d0829f15f8ae7b94d0638a
MSBear007 <igmedv2020@gmail.com>
2022-10-01 15:30:39 +0300

    test

--- testing/src/main/java/my/testing/app/App.java
+++ testing/src/main/java/my/testing/app/App.java
@@ -1,10 +1,8 @@
 package my.testing.app;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-/**
- * Hello world!
- *
- */
+
+
 public class App 
 {
     private static final Logger logger = LogManager.getLogger();
